0e2631332d368f60e9e36c954cb5257cf7aee73f
Edward Kemper <edwardkemper@gmail.com>
2017-08-03 12:41:52 -0600

    * .gitignore: * learning.sln: * Program.cs: * learning.csproj:
    * Moose.cs: rad

---
+++ learning/Moose.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace learning
+{
+	class Moose
+	{
+
+		public string Name;
+		public bool Antlers;
+		public string Email;
+
+		public static int Population;
+
+		public Moose(string name, bool antlers, string email)
+		{
+			Name = name;
+			Population = Population + 1;
+		}
+
+		// static int FeetToInches (int feet)
+		// {
+		//  int inches = feet * 12;
+		//  return inches;
+		// }
+	}
+}
+
+
+
